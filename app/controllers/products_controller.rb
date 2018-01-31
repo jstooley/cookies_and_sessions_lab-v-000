dab3b1a9178c17d867d1e8b4b0426cd7f2430097
Justin Tooley <jstooley823@gmail.com>
2018-01-31 01:45:17 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,13 @@ class ProductsController < ActionController::Base
   end
 
   def add
+    @item = Item.find(params[:id])
+
+
+  cart = session[:cart] || []
+  cart << @item.id
+
+  
+  session[:cart] = cart
   end
 end
